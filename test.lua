35b5cf403cae17b8758b4d37ab5bdf793a78ad14
kyedaley <kyeboshdaley@gmail.com>
2021-08-24 15:29:52 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 user = {
-    ["cb7847b38bac30857847d3e1c134b7f17d90e8dd6936eda45087de19412ca26c46aeffe7f75bbc9cee224f9481f4a836d7b33a5f46930d303878d82f840419c6"] = true
+    [""] = true
 }
 
 return user

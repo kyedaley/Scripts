ae83de9c317d57754ea5f9945d1a06800d4fcf12
kyedaley <kyeboshdaley@gmail.com>
2021-08-26 04:21:58 +0100

    Delete test.lua

--- test.lua
+++
@@ -1,7 +0,0 @@
-user = {
-    ["cb7847b38bac30857847d3e1c134b7f17d90e8dd6936eda45087de19412ca26c46aeffe7f75bbc9cee224f9481f4a836d7b33a5f46930d303878d82f840419c6"] = true,
-    ["CE96347CB5421F58D39832CC4AEE68C0CCF2"] = true
-}
-
-return user
-
